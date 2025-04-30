33688f4c7dec65145533eec97d3fa2efe1571f7c
Dieter Foos <d.foos@mail.de>
2025-04-30 22:24:19 +0200

    git update

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+const BASE_URL = "https://pokeapi.co/api/v2/";
 
+let myPokemon = [];
+let offset = 1;
+let limit = 18;
+function init() {
+  fetchPokemon(offset, limit);
+  document.getElementById('spinner').classList.add('spinner');
+  document.getElementById('loader').classList.add('loader');
+}
 
-
-const fetchPokemon = async (offset, limit) => {
-    for (let id = offset; id < offset + limit; id++) {
-      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-      const res = await fetch(url);
-      const data = await res.json();
-      myPokemon.push(data);
-      createCard(data);
+async function fetchPokemon(offset, limit) {
+  
+  for (let id = offset; id < offset + limit; id++) {
+    try {
+      const res = await fetch(BASE_URL + "pokemon/" + id);
+      const pokemon = await res.json();
+      const name = capitalize(pokemon.name);
+      const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+      const speciesResponse = await fetch(pokemon.species.url);
+      const speciesData = await speciesResponse.json();
+      const color = speciesData.color.name;
+      const types = pokemon.types.map((t) => t.type.name);
+      content.innerHTML += template(id, name, img, types, color);
+      myPokemon.push(id, name, img, types, color);
+    } catch {
+      content.innerHTML = "Versuche es später noch einmal";
     }
-  };
+  }
+  document.getElementById('spinner').classList.remove('spinner');
+  document.getElementById('loader').classList.remove('loader');
+  offset += limit;
+}
+
+
+
+capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
--- template.js
+++ template.js
@@ -1,18 +1,17 @@
-
-function getTemplate(name, id, img, types, color) {
-    let typeImg = types.map(type => 
-        `<img src="./assets/icons/${type}.svg" alt="${type}">`).join(" ");
-
-return `<div class="card">
-      <div class="title">
-        <h3>#${id}</h3>
+function template(id, name, img, types, color){ 
+  let typeImg = types.map(type => 
+    `<img src="./assets/icons/${type}.svg" alt="${type}">`).join(" ");
+  
+  return `
+  <div class="card">
+    <div class="title">
+      <h3>#${id}</h3>
         <h4>${name}</h4>
       </div>
-      <div class="${color}" class="image">
+      <div class="image ${color}">
         <img src="${img}" alt="" />
       </div>
-      <div class="icons">
-        ${typeImg}
-      </div>
-    </div>`
-}
+    <div class="icons">
+      ${typeImg}
+    </div>
+  </div>`};
